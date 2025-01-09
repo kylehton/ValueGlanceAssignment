f1ca843e903e95e4a51ec2f1396944447973cf32
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-01-08 18:22:50 -0800

    created buttons and table grid, formed sort/filter functions, need to implement

--- app/Table.js
+++ app/Table.js
@@ -1,29 +1,31 @@
 export function Table({ incomeStatements }) {
-
-return (
-    <table className="border-1 border-black">
-        <thead>
-          <tr className="w-full border-collapse">
-            <th className="text-center px-10 py-5">Date</th>
-            <th className="text-center px-10 py-5">Revenue</th>
-            <th className="text-center px-10 py-5">Net Income</th>
-            <th className="text-center px-10 py-5">Gross Profit</th>
-            <th className="text-center px-10 py-5">EPS</th>
-            <th className="text-center px-10 py-5">Operating Income</th>
-          </tr>
-        </thead>
-        <tbody>
-        {incomeStatements.map((incomeStatement, index) => (
-          <tr key={index}>
-            <td className="text-center">{incomeStatement[0]}</td>
-            <td className="text-center">{incomeStatement[1]}</td>
-            <td className="text-center">{incomeStatement[2]}</td>
-            <td className="text-center">{incomeStatement[3]}</td>
-            <td className="text-center">{incomeStatement[4]}</td>
-            <td className="text-center">{incomeStatement[5]}</td>
-          </tr>
-        ))}
-        </tbody>
-      </table>
-);
-}
+    return (
+      <div className="mt-10 overflow-x-auto min-w-full">
+        <table className="table-auto border-collapse border border-black min-w-full sm:w-auto">
+          <thead>
+            <tr>
+              <th className="text-center px-3 py-2 border border-black">Date</th>
+              <th className="text-center px-3 py-2 border border-black">Revenue</th>
+              <th className="text-center px-3 py-2 border border-black">Net Income</th>
+              <th className="text-center px-3 py-2 border border-black">Gross Profit</th>
+              <th className="text-center px-3 py-2 border border-black">EPS</th>
+              <th className="text-center px-3 py-2 border border-black">Operating Income</th>
+            </tr>
+          </thead>
+          <tbody>
+            {incomeStatements.map((incomeStatement, index) => (
+              <tr key={index}>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[0]}</td>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[1]}</td>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[2]}</td>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[3]}</td>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[4]}</td>
+                <td className="text-center px-3 py-2 border border-black">{incomeStatement[5]}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+  
