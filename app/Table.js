56899a9f265c5bbdc1af7a2a9dcc3a6bfffaacc7
Kyle Ton <kyleton@Kyles-MacBook-Air-M3.local>
2025-01-10 22:08:43 -0800

    made only one border for table, and attempt to center buttons and table when in mobile view

--- app/Table.js
+++ app/Table.js
@@ -2,7 +2,7 @@ import "./globals.css";
 
 export function Table ({ incomeStatements }) {
     return (
-      <div className="mt-5 mb-10 overflow-x-auto overflow-y-auto rounded border border-zinc-300 text-zinc-600"
+      <div className="m-5 mb-10 overflow-x-auto overflow-y-auto rounded border border-white text-zinc-600"
       style={{
         maxWidth: "95%",
         marginLeft: 20,
@@ -18,23 +18,23 @@ export function Table ({ incomeStatements }) {
         >
           <thead>
             <tr>
-              <th className="text-center px-2 py-3 border border-gray-300">Date</th>
-              <th className="text-center px-2 py-3 border border-gray-300">Revenue</th>
-              <th className="text-center px-2 py-3 border border-gray-300">Net Income</th>
-              <th className="text-center px-2 py-3 border border-gray-300">Gross Profit</th>
-              <th className="text-center px-2 py-3 border border-gray-300">EPS</th>
-              <th className="text-center px-2 py-3 border border-gray-300">Operating Income</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">Date</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">Revenue</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">Net Income</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">Gross Profit</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">EPS</th>
+              <th className="text-center px-2 py-3 border border-zinc-300">Operating Income</th>
             </tr>
           </thead>
           <tbody>
             {incomeStatements.map((incomeStatement, index) => (
               <tr key={index}>
-                <td className="text-center px-2 py-2 border border-gray-300">{(incomeStatement[0].toString()).substring(0,15)}</td>
-                <td className="text-center px-2 py-2 border border-gray-300">{incomeStatement[1]}</td>
-                <td className="text-center px-2 py-2 border border-gray-300">{incomeStatement[2]}</td>
-                <td className="text-center px-2 py-2 border border-gray-300">{incomeStatement[3]}</td>
-                <td className="text-center px-2 py-2 border border-gray-300">{incomeStatement[4]}</td>
-                <td className="text-center px-2 py-2 border border-gray-300">{incomeStatement[5]}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{(incomeStatement[0].toString()).substring(0,15)}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{incomeStatement[1]}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{incomeStatement[2]}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{incomeStatement[3]}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{incomeStatement[4]}</td>
+                <td className="text-center px-2 py-2 border border-zinc-300">{incomeStatement[5]}</td>
               </tr>
             ))}
           </tbody>
